47aa3cd1f62c9b0c231d33cb17a897519f55b7f8
DaanKwF <daan@kashmirworldfoundation.org>
2023-06-09 18:35:47 +0200

    Display platform icons

--- src/GameHub/components/GameCard.tsx
+++ src/GameHub/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../../hooks/useGames";
 import useImage from "../../hooks/useImage";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
@@ -15,6 +16,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={image64} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
