0954d5361dca9795f6336285f043edd39496149f
DaanKwF <daan@kashmirworldfoundation.org>
2023-06-11 18:28:38 +0200

    Build Search Input

--- src/GameHub/components/NavBar.tsx
+++ src/GameHub/components/NavBar.tsx
@@ -1,11 +1,13 @@
 const logo = require("../../assets/logo.webp");
 import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding={10}>
+    <HStack padding="10px">
       <Image src={logo} boxSize={16} />
+      <SearchBar />
       <ColorModeSwitch />
     </HStack>
   );
